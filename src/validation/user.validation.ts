fed5da771e81f3dd8f801c7328f03bfb4be22563
kaleab Teweldbrhan Abay <42609047+kaleabteweld@users.noreply.github.com>
2024-09-22 12:58:41 +0300

    EditUserInfo init

--- src/validation/user.validation.ts
+++ src/validation/user.validation.ts
@@ -29,6 +29,24 @@ export const signUpSchema = z.object({
     confirm_password: undefined,
 }));
 
+export const updateUserSchema = z.object({
+    email: z.string()
+        .email({ message: "Please enter a valid email address." })
+        .optional(),
+    first_name: z.string().optional(),
+    last_name: z.string().optional(),
+    phone_number: z.string().optional(),
+    profile_img: z
+        .any()
+        .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+        .refine(
+            (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+            "Only .jpg, .jpeg, .png and .webp formats are supported."
+        ),
+}).transform((obj) => ({
+    ...obj,
+}));
+
 export const logInSchema = z.object({
     email: z.string()
         .email({ message: "Please enter a valid email address." })
